1ac83f7dd1ecd7a669a931bbd9854288c3f9567e
RidwanRamdhani <gtiridwan@gmail.com>
2026-07-25 01:17:21 +0700

    feat: refactor AuthController

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
