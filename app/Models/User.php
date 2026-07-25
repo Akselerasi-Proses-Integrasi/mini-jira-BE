eebed9b2152a30ad1a2e97da750f53558efdbdcf
SanubariLegawa <sayamemanggila1210@gmail.com>
2026-07-25 15:24:53 +0700

    feat: Add Google Login

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'nama',
         'email',
         'password',
+        'google_id',
     ];
 
     protected $hidden = [
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login/google', [AuthController::class, 'googleLogin']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']); 
